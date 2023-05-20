699afd3baf686ab5308dd4f04c3632d4f4062488
David <davidtrufanov@mail.ru>
2023-05-21 01:13:45 +0700

    override info method and change plots

--- simulations/training_simulation.py
+++ simulations/training_simulation.py
@@ -2,7 +2,8 @@ import os
 import sys
 import gymnasium as gym
 import argparse
-import sumo_rl
+from sumo_rl import SumoEnvironment
+import numpy as np
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -50,5 +51,25 @@ def create_2single_env(use_gui=False, num_seconds=3500, out_csv_name=args.out_cs
     return env
 
 
+class MySumoEnvironment(SumoEnvironment):
+    def _get_system_info(self):
+        vehicles = self.sumo.vehicle.getIDList()
+        speeds = [self.sumo.vehicle.getSpeed(vehicle) for vehicle in vehicles]
+        waiting_times = [self.sumo.vehicle.getWaitingTime(vehicle) for vehicle in vehicles]
+        return {
+            # In SUMO, a vehicle is considered halting if its speed is below 0.1 m/s
+            # add system_running_cars to info dictionary
+            "system_total_stopped": sum(int(speed < 0.1) for speed in speeds),
+            "system_total_waiting_time": sum(waiting_times),
+            "system_mean_waiting_time": 0.0 if len(vehicles) == 0 else np.mean(waiting_times),
+            "system_mean_speed": 0.0 if len(vehicles) == 0 else np.mean(speeds),
+            "system_running_cars": len(vehicles)
+        }
+
+
 if __name__ == '__main__':
-    pass
+    env = MySumoEnvironment(args.net_file_2signle, args.route_file, single_agent=True, num_seconds=3000)
+    env.reset()
+    while True:
+        next_obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        print(env._get_system_info())
--- utils/plots.py
+++ utils/plots.py
@@ -1,23 +1,49 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+from simulations.training_simulation import MySumoEnvironment
+from simulations.training_simulation import args
+from scipy import interpolate
+import numpy as np
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 
 
-def plot_history(csv_file_path: str, system_info=True):
+# TODO: system_total_stopped system_mean_waiting_time system_mean_speed running_cars
+#       добавить kde графики
+
+def plot_history(csv_file_path: str):
     data = pd.read_csv(csv_file_path)
-    if system_info:
-        fig, axes = plt.subplots(2, 2, figsize=(15, 6))
-        sns.lineplot(data=data, x='step', y='system_total_stopped', ax=axes[0, 0])
-        sns.lineplot(data=data, x='step', y='system_total_waiting_time', ax=axes[0, 1])
-        sns.lineplot(data=data, x='step', y='system_mean_waiting_time', ax=axes[1, 0])
-        sns.lineplot(data=data, x='step', y='system_mean_speed', ax=axes[1, 1])
-        sns.lineplot(data=data, x='step', y=data['system_mean_speed'].mean(), ax=axes[1, 1])
-        fig.tight_layout()
-        plt.show()
+    fig, axes = plt.subplots(2, 2, figsize=(15, 6))
+    sns.lineplot(data=data, x='step', y='system_total_stopped', ax=axes[0, 0], label='system_total_stopped')
+    sns.lineplot(x=data['step'], y=[data['system_total_stopped'].median()] * len(data['step']),
+                 linestyle='--', ax=axes[0, 0], label='median')
+    sns.lineplot(data=data, x='step', y='system_running_cars', ax=axes[0, 1], label='system_running_cars')
+    sns.lineplot(x=data['step'], y=[data['system_running_cars'].median()] * len(data['step']),
+                 linestyle='--', ax=axes[0, 1], label='median')
+    sns.lineplot(data=data, x='step', y='system_mean_waiting_time', ax=axes[1, 0], label='system_mean_waiting_time')
+    sns.lineplot(x=data['step'], y=[data['system_mean_waiting_time'].median()] * len(data['step']),
+                 linestyle='--', ax=axes[1, 0], label='median')
+    sns.lineplot(data=data, x='step', y='system_mean_speed', ax=axes[1, 1], label='system_mean_speed')
+    sns.lineplot(x=data['step'], y=[data['system_mean_speed'].median()] * len(data['step']),
+                 linestyle='--', ax=axes[1, 1], label='median')
+    fig.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
-    plot_history(r'C:\Users\sskil\PycharmProjects\course_work\saved_history\A3C_history\history.csv_conn4_run3.csv', True)
+    env = MySumoEnvironment(args.net_file_2signle, args.route_file, single_agent=True, num_seconds=1000,
+                            add_per_agent_info=False, out_csv_name=args.out_csv_name)
+    obs, info = env.reset()
+    for key, value in info.items():
+        info.update({key: [value]})
+    test_df = pd.DataFrame(info)
+    done = False
+    while not done:
+        next_obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+        for key, value in info.items():
+            info.update({key: [value]})
+        output = pd.DataFrame(info)
+        test_df = pd.concat([test_df, output], ignore_index=True)
+        done = terminated or truncated
